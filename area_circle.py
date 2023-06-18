fcc87eca42d15fce8bbb1b24d9aef80abc32467a
Himanshi <132876954+Himanshijain18@users.noreply.github.com>
2023-06-18 23:27:29 +0530

    Add files via upload

---
+++ area_circle.py
@@ -0,0 +1,8 @@
+#area of circle
+
+import math
+radius = float(input("Input the radius of the cicle : "))
+
+area = math.pi * radius *radius
+
+print("The Area of the circle with radius "+ str(radius) +" is : {0}".format(area))
