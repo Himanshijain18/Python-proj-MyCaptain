bc7e3a8e3580f63adfeb971a9920ae26af82f295
Himanshi <132876954+Himanshijain18@users.noreply.github.com>
2023-06-22 15:16:28 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+#Fibonacci series
+n=int(input("Enter the no. till you want the series- "))
+x=0
+y=1
+z=0
+while z<=n:
+    print(z)
+    x=y
+    y=z
+    z=x+y
---
+++ positive_no.py
@@ -0,0 +1,11 @@
+#Positive numbers
+list1 = [12, -7, 5, 64, -14]
+for i in list1:
+     if i>=0:
+         print(i,' ',end='')
+list2 = [12, 14, -95, 3]
+print("\n")
+for i in list2:
+     if i>=0:
+       
+         print(i,' ',end='')
